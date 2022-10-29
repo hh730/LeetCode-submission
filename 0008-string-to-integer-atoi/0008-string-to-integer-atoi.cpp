fdb7b60679cbff13942f450a42680e71dfe1b4f6
Hardik Gupta <51542809+hh730@users.noreply.github.com>
2022-10-30 01:52:05 +0530

    Time: 7 ms (51.53%), Space: 7 MB (52.54%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        const int len = s.size();
+        
+        if(len == 0){
+            return 0;
+        }
+        
+        int index = 0;
+        while(index < len && s[index] == ' '){
+            ++index;
+        }
+
+        if(index == len){
+            return 0;
+        }
+        
+        char ch;
+        bool isNegative = (ch = s[index]) == '-';
+        
+        if(isNegative || ch == '+'){
+            ++index;
+        }
+		
+        const int maxLimit = INT_MAX / 10;
+        int result = 0;
+        while(index < len && s[index]>='0'&&s[index]<='9'){ 
+
+            int digit = s[index] - '0';
+
+            if(result > maxLimit || (result == maxLimit && digit > 7)){
+                return isNegative ? INT_MIN : INT_MAX;
+            }
+
+            result = (result * 10) + digit;
+            
+            ++index;
+        }
+        
+        return isNegative ? -result : result;
+
+    }
+};
