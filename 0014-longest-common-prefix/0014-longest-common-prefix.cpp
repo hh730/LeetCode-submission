46261fb56bdd91f23e283db07dc52e7d1812eba4
Hardik Gupta <51542809+hh730@users.noreply.github.com>
2022-10-31 01:27:52 +0530

    Time: 10 ms (44.98%), Space: 9.2 MB (53.70%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int minLen=INT_MAX;
-        for(int i=0;i<strs.size();i++){
-            if(strs[i].length()<minLen){
-                minLen=strs[i].length();
+        string res = "";
+        
+        sort(strs.begin(), strs.end());
+        
+        string first = strs[0];
+        int n = first.length();
+        string last = strs[strs.size()-1];
+        
+        for(int i=0; i<n; i++){
+            if(first[i] == last[i]){
+                res += first[i];
             }
-        }
-        string ans="";
-        bool stop=false;
-        for(int i=0;i<minLen;i++){
-            char ch=strs[0][i];
-            for(int j=1;j<strs.size();j++){
-                if(strs[j][i]!=ch){
-                    stop=true;
-                    break;
-                }
-            }
-            if(stop){
+            else{
                 break;
             }
-            ans=ans+ch;
         }
-        return ans;
+        return res;
+        // int minLen=INT_MAX;
+        // for(int i=0;i<strs.size();i++){
+        //     if(strs[i].length()<minLen){
+        //         minLen=strs[i].length();
+        //     }
+        // }
+        // string ans="";
+        // bool stop=false;
+        // for(int i=0;i<minLen;i++){
+        //     char ch=strs[0][i];
+        //     for(int j=1;j<strs.size();j++){
+        //         if(strs[j][i]!=ch){
+        //             stop=true;
+        //             break;
+        //         }
+        //     }
+        //     if(stop){
+        //         break;
+        //     }
+        //     ans=ans+ch;
+        // }
+        // return ans;
     }
 };
