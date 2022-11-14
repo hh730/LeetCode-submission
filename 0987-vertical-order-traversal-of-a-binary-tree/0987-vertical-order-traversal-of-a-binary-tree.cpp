cf7a505f24ce1c2e867066468a39fc863f6f0091
Hardik Gupta <51542809+hh730@users.noreply.github.com>
2022-11-15 01:09:31 +0530

    Time: 8 ms (63.33%), Space: 13.6 MB (34.49%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -38,6 +38,30 @@ public:
             ans.push_back(col);
         }
         return ans;
-        return ans;
+        //recursive
+//     int min = INT_MAX;
+//     int max = INT_MIN;
+//   void helper(TreeNode* root, unordered_map<int,vector<pair<int,int>>> &m, int i, int h){
+//         if(root==NULL)return;
+//         m[i].push_back(make_pair(h,root->val));
+//         if(i<min)min=i;
+//         if(i>max)max=i;
+//         helper(root->left,m,i-1,h+1);
+//         helper(root->right,m,i+1,h+1);
+//     }
+
+//     vector<vector<int>> verticalTraversal(TreeNode* root) {
+//         unordered_map<int,vector<pair<int,int>>> m;
+//         helper(root,m,0,0);
+//         vector<vector<int>> ans;
+//         for(int i=min;i<=max;i++){
+//             sort(m[i].begin(),m[i].end());
+//             vector<int> v;
+//             for(int j=0;j<m[i].size();j++){
+//                 v.push_back(m[i][j].second);
+//             }
+//             ans.push_back(v);
+//         } 
+//         return ans;
     }
 };
