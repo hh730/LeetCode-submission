e3625d8b8f2a118b174c46944643fc224877916d
Hardik Gupta <51542809+hh730@users.noreply.github.com>
2022-12-07 01:41:31 +0530

    Time: 375 ms (56.50%), Space: 159.9 MB (10.16%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    bool equalHelper(vector<int>& arr,int N,int sum,vector<vector<int>>&dp){
-    if(sum==0){
-        return true;
-    }
-    if(N==0){
-        return (sum==0);
-    }
-    if(dp[N][sum]!=-1){
-        return dp[N][sum];
-    }
-        if(arr[N-1]<=sum){
-    dp[N][sum]= equalHelper(arr,N-1,sum-arr[N-1],dp)||equalHelper(arr,N-1,sum,dp);
-        }
-        else{
-            dp[N][sum]=equalHelper(arr,N-1,sum,dp);
-        }
-    return dp[N][sum];
-}
+    
+    //memoized code
+    
+//     bool equalHelper(vector<int>& arr,int N,int sum,vector<vector<int>>&dp){
+//     if(sum==0){
+//         return true;
+//     }
+//     if(N==0){
+//         return (sum==0);
+//     }
+//     if(dp[N][sum]!=-1){
+//         return dp[N][sum];
+//     }
+//         if(arr[N-1]<=sum){
+//             dp[N][sum]= equalHelper(arr,N-1,sum-arr[N-1],dp)||equalHelper(arr,N-1,sum,dp);
+//         }
+//         else{
+//             dp[N][sum]=equalHelper(arr,N-1,sum,dp);
+//         }
+//     return dp[N][sum];
+// }
     bool canPartition(vector<int>& arr) {
         int N=arr.size();
         int sum=0;
@@ -27,9 +30,30 @@ public:
         if(sum%2!=0){
             return false;
         }
-        int temp=sum;
         vector<vector<int>>dp(N+1,vector<int>(sum+1,-1));
-        equalHelper(arr,N,sum/2,dp);
+        
+        //memoized code
+        // equalHelper(arr,N,sum/2,dp);
+        // return dp[N][sum/2];
+        
+        //Top down approach
+        
+        for(int i=0;i<N+1;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<sum+1;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<N+1;i++){
+            for(int j=1;j<(sum/2)+1;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
         return dp[N][sum/2];
     }
 };
