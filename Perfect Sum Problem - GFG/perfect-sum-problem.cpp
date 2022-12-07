71684c5ea5a760d74166261d1d550956b1331bef
Hardik Gupta <51542809+hh730@users.noreply.github.com>
2022-12-07 16:13:12 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -13,14 +13,13 @@ class Solution{
         int mod=1000000007;
         vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
         for(int i=0;i<n+1;i++){
-            if(arr[0]==0&&sum==0){
+            if(arr[i]==0&&sum==0){
                 dp[i][0]=2;
             }
             else{
             dp[i][0]=1;
             }
         }
-        // if(arr[0]<=sum) dp[0][arr[0]] = 1;
         for(int i=1;i<n+1;i++){
             for(int j=0;j<sum+1;j++){
                 if(arr[i-1]<=j){
