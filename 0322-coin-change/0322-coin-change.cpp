e38c2f95c460b4f2be40c20c7b753ca6fef65472
Hardik Gupta <hardikgarg1999@gmail.com>
2022-12-09 01:07:53 +0530

    Time: 178 ms (45.49%), Space: 49.4 MB (12.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,21 +4,36 @@ public:
         int n=coins.size();
         vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
         int sum=amount;
+        //Initialization
+        //When n==0 (no coins) then we need infinite amount of coin to make sum=amount
+        // if amount==0 then we need zero coin;
+        
         for(int i=0;i<sum+1;i++){
             dp[0][i]=INT_MAX-1;
         }
         for(int i=1;i<n+1;i++){
             dp[i][0]=0;
         }
+        
+        // Now we have to initialize 2nd row also
+        // in 2nd row we have size ==1
+        // so if a j%coins[0]==0 then t[1][j]=j/coins[0];
+        // else INT_MAX
+        
         for(int i=1;i<sum+1;i++){
-            if(coins[0]%i==0){
-                dp[1][i]=coins[0]/i;
+            if(i%coins[0]==0){
+                dp[1][i]=i/coins[0];
             }
             else{
                 dp[1][i]=INT_MAX-1;
             }
         }
-        for(int i=1;i<n+1;i++){
+        
+        // i will start from 2 as we have filled/initialised first 2 rows already
+        //Now we need to fill our tabel
+        // We have two choice take or reject
+        
+        for(int i=2;i<n+1;i++){
             for(int j=1;j<sum+1;j++){
                 int incl=INT_MAX-1;
                 int excl=dp[i-1][j];
@@ -28,6 +43,7 @@ public:
                 dp[i][j]=min(incl,excl);
             }
         }
+        // as if not possible we need to retutn -1
         if(dp[n][sum]==INT_MAX||dp[n][sum]==INT_MAX-1){
             return -1;
         }
