1d1f4356b4d33a67afa9d99d3d188a52214131cd
Hardik Gupta <hardikgarg1999@gmail.com>
2023-02-16 00:01:52 +0530

    Time: 28 ms (79.50%), Space: 28.6 MB (33.76%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        vector<int>ans;
-        int carry=0;
-        int i=num.size()-1;
-        while(i>=0){
-            int sum=num[i]+(k%10)+carry;
-            ans.push_back(sum%10);
-            carry=sum/10;
-            k=k/10;
-            i--;
+        // vector<int>ans;
+        // int carry=0;
+        // int i=num.size()-1;
+        // while(i>=0){
+        //     int sum=num[i]+(k%10)+carry;
+        //     ans.push_back(sum%10);
+        //     carry=sum/10;
+        //     k=k/10;
+        //     i--;
+        // }
+        // while(k!=0){
+        //     int sum=k%10+carry;
+        //     ans.push_back(sum%10);
+        //     carry=sum/10;
+        //     k=k/10;
+        // }
+        // if(carry==1){
+        //     ans.push_back(carry);
+        // }
+        // reverse(ans.begin(),ans.end());
+        // return ans;
+        vector<int> ans;
+        int n = num.size();
+        int carry = 0, i = n-1;
+        while (k > 0 || i >= 0 || carry > 0) {
+            int sum = carry;
+            if (k > 0) {
+                int remainder = k % 10;
+                sum += remainder;
+                k = k / 10;
+            }
+            if (i >= 0) {
+                sum += num[i];
+                i--;
+            }
+            carry = sum / 10;
+            ans.push_back(sum % 10);
         }
-        while(k!=0){
-            int sum=k%10+carry;
-            ans.push_back(sum%10);
-            carry=sum/10;
-            k=k/10;
-        }
-        if(carry==1){
-            ans.push_back(carry);
-        }
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
